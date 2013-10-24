160afbd6016f256f6f0f8615f0df6da093b1379f
benet432 <lishuo.app@gmail.com>
2013-10-23 22:24:17 -0700

    gaga

--- HW1/BayesLogit/BLR_fit.R
+++ HW1/BayesLogit/BLR_fit.R
@@ -1,7 +1,56 @@
+
+##
+#
+# Logistic regression
+# 
+# Y_{i} | \beta \sim \textrm{Bin}\left(n_{i},e^{x_{i}^{T}\beta}/(1+e^{x_{i}^{T}\beta})\right)
+# \beta \sim N\left(\beta_{0},\Sigma_{0}\right)
+#
+##
+
+library(mvtnorm)
 library(MASS)
+library(coda)
+
+########################################################################################
+########################################################################################
+## Handle batch job arguments:
+
+# 1-indexed version is used now.
+args <- commandArgs(TRUE)
+
+cat(paste0("Command-line arguments:\n"))
+print(args)
+
+####
+# sim_start ==> Lowest simulation number to be analyzed by this particular batch job
+###
+
+#######################
+sim_start <- 1000
+length.datasets <- 200
+#######################
+
+if (length(args)==0){
+  sinkit <- FALSE
+  sim_num <- sim_start + 1
+  set.seed(1330931)
+} else {
+  # Sink output to file?
+  sinkit <- TRUE
+  # Decide on the job number, usually start at 1000:
+  sim_num <- sim_start + as.numeric(args[1])
+  # Set a different random seed for every job number!!!
+  set.seed(762*sim_num + 1330931)
+}
+
+# Simulation datasets numbered 1001-1200
+
+########################################################################################
+########################################################################################
 
 bayes1= function(m, y, X, beta.star, beta.0, Sigma.0.inv)
-
+  
 {
   n= length(m)
   p= ncol(X)
@@ -9,7 +58,9 @@ bayes1= function(m, y, X, beta.star, beta.0, Sigma.0.inv)
   return(pi)
 }
 
-bayes.logreg = function(m, y, X, beta.0=c(0, 0), Sigma.0.inv=diag(1, 2), niter=10000, burnin=1000, print.every=1000, retune=200, verbose=TRUE)
+
+"bayes.logreg" <- function(n,y,X,beta.0,Sigma.0.inv,niter=10000,burnin=1000,
+                           print.every=1000,retune=100,verbose=TRUE)
 {
   beta_ci=matrix(numeric(2*99), 99, 2)
   v_2=0.01
@@ -67,14 +118,42 @@ bayes.logreg = function(m, y, X, beta.0=c(0, 0), Sigma.0.inv=diag(1, 2), niter=1
   }
   beta=beta[((burnin+2):(burnin+niter+1)), ]
   for (j in 1:2)
-  beta_ci[, j]=quantile(beta[, j], probs=seq(0.01, 0.99, 0.01))
+    beta_ci[, j]=quantile(beta[, j], probs=seq(0.01, 0.99, 0.01))
   write.table(beta_ci, file=paste('results/blr_res_', as.character(sim_num),'.csv', sep=""), sep=",", row.names = FALSE, col.names = FALSE)
   
 }
 
+#################################################
+# Set up the specifications:
 beta.0 <- matrix(c(0,0))
-Sigma.0.inv <- diag(rep(1, 2))
+Sigma.0.inv <- diag(rep(1.0,2))
+niter <- 10000
+
 data=read.csv(file=paste('data/blr_data_', as.character(sim_num), '.csv', sep=""), header=FALSE, sep=",")
 m=data$n
 y=data$y
 X=as.matrix(data[, 3:4])
+
+# etc... (more needed here)
+#################################################
+
+# Read data corresponding to appropriate sim_num:
+
+# Extract X and y:
+
+# Fit the Bayesian model:
+
+# Extract posterior quantiles...
+
+# Write results to a (99 x p) csv file...
+
+# Go celebrate.
+ 
+cat("done. :)\n")
+
+
+
+
+
+
+
