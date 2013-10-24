936a855f74d3f260e8c1d707e6caf65992147f22
benet432 <lishuo.app@gmail.com>
2013-10-23 21:56:19 -0700

    qqq

--- HW1/BayesLogit/BLR_fit_skeleton.R
+++ HW1/BayesLogit/BLR_fit_skeleton.R
@@ -53,13 +53,71 @@ if (length(args)==0){
 "bayes.logreg" <- function(n,y,X,beta.0,Sigma.0.inv,niter=10000,burnin=1000,
                            print.every=1000,retune=100,verbose=TRUE)
 {
-	# Stuff
+  beta_ci=matrix(numeric(2*99), 99, 2)
+  v_2=0.01
+  Sigma_new=v_2*diag(1, 2)
+  beta=matrix(numeric(2*(burnin+niter+1)), (burnin+niter+1), 2)
+  beta[1,]= beta.0
+  n= length(m)
+  p= ncol(X)
+  U=runif((burnin+niter), 0, 1)
+  n=0
+  while (verbose)
+  {
+    for (i in 2: (retune+1))
+    {
+      
+      beta[i, ]=mvrnorm(1, mu=beta[i-1, ], Sigma=Sigma_new)
+      alpha=bayes1(m, y, X, beta[i, ], beta.0, Sigma.0.inv)-bayes1(m, y, X, beta[i-1, ], beta.0, Sigma.0.inv)
+      if (log(U[i-1])<alpha)
+      { beta[i, ]=beta[i, ] 
+        n=n+1
+      }
+      else
+      { beta[i, ]=beta[i-1, ] }     
+    }  
+    
+    accept_rate=n/retune
+    n=0
+    
+    if (accept_rate>=0.3 & accept_rate<=0.6)
+    { 
+      verbose = FALSE
+      cat("Accept Rate is: ",accept_rate," We take v^2 as: ", Sigma_new[1, 1], "\n")
+      for (i in (2+retune): (burnin+niter+1))
+      {
+        beta[i, ]=mvrnorm(1, mu=beta[i-1, ], Sigma=Sigma_new)
+        alpha=bayes1(m, y, X, beta[i, ], beta.0, Sigma.0.inv)-bayes1(m, y, X, beta[i-1, ], beta.0, Sigma.0.inv)
+        if (log(U[i-1])<alpha)
+        { beta[i, ]=beta[i, ] }
+        else
+        { beta[i, ]=beta[i-1, ] }       
+      }  
+    }
+    else if (accept_rate<0.3)
+    {
+      verbose = TRUE
+      Sigma_new=Sigma_new*exp(-0.5)
+      cat(cat("Accept Rate is: ",accept_rate," we adjust v^2 to: ", Sigma_new[1, 1], "\n"))
+    }
+    else
+    {
+      verbose = TRUE
+      Sigma_new=Sigma_new*exp(0.5)
+      cat(cat("Accept Rate is: ",accept_rate," we adjust v^2 to: ", Sigma_new[1, 1], "\n"))
+    }  
+  }
+  beta=beta[((burnin+2):(burnin+niter+1)), ]
+  for (j in 1:2)
+    beta_ci[, j]=quantile(beta[, j], probs=seq(0.01, 0.99, 0.01))
+  write.table(beta_ci, file=paste('results/blr_res_', as.character(sim_num),'.csv', sep=""), sep=",", row.names = FALSE, col.names = FALSE)
+  
 }
 
 #################################################
 # Set up the specifications:
 beta.0 <- matrix(c(0,0))
-Sigma.0.inv <- diag(rep(1.0,p))
+Sigma.0.inv <- diag(rep(1.0,2))
 niter <- 10000
 # etc... (more needed here)
 #################################################
