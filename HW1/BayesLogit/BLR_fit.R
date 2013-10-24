3103ac533409e7872c2371bf13438ad0a0a6270e
benet432 <lishuo.app@gmail.com>
2013-10-23 23:01:09 -0700

    afa

--- HW1/BayesLogit/BLR_fit.R
+++ HW1/BayesLogit/BLR_fit.R
@@ -127,6 +127,7 @@ data=read.csv(file=paste('data/blr_data_', as.character(sim_num), '.csv', sep=""
 m=data$n
 y=data$y
 X=as.matrix(data[, 3:4])
+p=ncol(X)
 beta.0 = matrix(c(0,0))
 Sigma.0.inv = diag(rep(1.0,p))
 niter = 10000
