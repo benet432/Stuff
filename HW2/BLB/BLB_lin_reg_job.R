7f943928525de123c866f0e77548ff5470bc3d39
benet432 <lishuo.app@gmail.com>
2013-11-11 23:38:26 -0800

    hw2

--- HW2/BLB/BLB_lin_reg_job.R
+++ HW2/BLB/BLB_lin_reg_job.R
@@ -30,6 +30,17 @@ cat(paste("\nAnalyzing dataset number ",sim_num,"...\n\n",sep=""))
 
 # Find r and s indices:
 
+jobnum=sim_num-1000
+if (jobnum %% 50 ==0)
+{
+  s_index=jobnum/50
+  r_index=50
+}else
+{
+  s_index=as.integer(jobnum/50)+1
+  r_index=jobnum %% 50
+}
+
 #============================== Run the simulation study ==============================#
 
 # Load packages:
@@ -38,9 +49,6 @@ library(bigmemory.sri)
 library(bigmemory)
 library(biganalytics)
 
-# I/O specifications:
-datapath <- "/home/pdbaines/data"
-outpath <- "output/"
 
 # mini or full?
 if (mini){
@@ -49,14 +57,30 @@ if (mini){
 	rootfilename <- "blb_lin_reg_data"
 }
 
-# Filenames:
-# Set up I/O stuff:
 # Attach big.matrix :
-# Remaining BLB specs:
-# Extract the subset:
-# Reset simulation seed:
-# Bootstrap dataset:
-# Fit lm:
-# Output file:
-# Save estimates to file:
+dat= attach.big.matrix(dget("/home/pdbaines/data/blb_lin_reg_data.desc"), backingpath="/home/pdbaines/data/")
 
+# Remaining BLB specs:
+
+n=nrow(dat)
+s = 5
+r = 50
+gamma = 0.7
+b=round(n^gamma)
+
+#sample b rows for the five subsets
+set.seed(s_index)
+index1=sample(1:n, b, replace = FALSE)
+
+#sample of size n
+set.seed(jobnum)
+index2=rmultinom(1, n, prob=rep(1/b, b))
+
+# Extract the subset:
+data=data.frame(dat[index1, ])
+
+# Fit lm:
+coefficient=lm(X1001~.-1, data, weights=index2)$coefficient
+
+# Output file & Save estimates to file:
+write.table(coefficient, file = paste0("output/","coef_",sprintf("%02d",s_index),"_",sprintf("%02d",r_index),".txt"))
